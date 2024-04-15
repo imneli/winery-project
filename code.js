fafb15422d3cf96b36dea0e3209a659355d87ce5
neli <154631371+imneli@users.noreply.github.com>
2024-04-15 16:42:03 -0300

    Delete code.js

--- code.js
+++
@@ -1,9 +0,0 @@
-function idade(resposta) {  /*JS PARA O POPUP*/
-    /*COMPARA AS RESPOSTAS DO USUÁRIO*/
-    if(resposta === 'sim'){
-        document.getElementById('popup').style.display = 'none';
-    }
-    else if(resposta === 'nao'){
-        window.location.href = 'https://www.google.com.br';
-    }
-}
